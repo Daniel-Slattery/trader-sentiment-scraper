e005231d90f27af9c2ccb298b2f8dfcb495055a3
Daniel Slattery <daniel.slattery1@gmail.com>
2023-06-30 22:37:37 +0100

    added is_forex_market_open function and set script to scrape at 55mins

--- scraper.py
+++ scraper.py
@@ -3,9 +3,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from utilities import is_forex_market_open
 import time
 import csv
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import platform
 
@@ -15,22 +16,7 @@ url = os.environ.get('SENTIMENT_URL')
 # Setup the driver. This is the part that opens up a browser
 driver = None
 
-# Detect the platform
-system = platform.system()
-if system == 'Windows':
-    # Use Brave on Windows
-    brave_path = 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe'
-    options = Options()
-    options.binary_location = brave_path
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-elif system == 'Darwin':
-    # Use Chrome on macOS
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-else:
-    print("Unsupported platform:", system)
-    exit(1)
-
-def scrape():
+def scrape(driver):
     # Make a request to the website
     driver.get(url)
 
@@ -41,7 +27,9 @@ def scrape():
     headers = [header.text for header in driver.find_elements(By.CLASS_NAME, 'SentimentRowCaption')]
     values = [value.text for value in driver.find_elements(By.CLASS_NAME, 'SentimentValueCaptionLong')]
 
-    data_row = [str(datetime.now())] + values
+    # Get the next hour as the timestamp
+    next_hour = (datetime.now() + timedelta(hours=1)).replace(minute=0, second=0)
+    data_row = [next_hour.strftime('%Y-%m-%d %H:%M')] + values
 
     with open('Sentiment.csv', 'a', newline='') as f:
         writer = csv.writer(f)
@@ -51,8 +39,31 @@ def scrape():
         writer.writerow(data_row)  # adjust this depending on the structure of 'item'
 
 while True:
-    scrape()
-    time.sleep(3600)  # Wait for 1 hour
+    if is_forex_market_open():
+        # Get current minutes
+        current_min = datetime.now().minute
+        # If it's 5 minutes to the next hour
+        if current_min == 55:
+            # Check if the driver is None
+            if driver is None:
+                # Detect the platform
+                system = platform.system()
+                if system == 'Windows':
+                    # Use Brave on Windows
+                    brave_path = 'C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe'
+                    options = Options()
+                    options.binary_location = brave_path
+                    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+                elif system == 'Darwin':
+                    # Use Chrome on macOS
+                    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+                else:
+                    print("Unsupported platform:", system)
+                    exit(1)
 
-# Remember to close the driver when done
-driver.quit()
+            scrape(driver)
+            # Remember to close the driver when done
+            driver.quit()
+            time.sleep(300)  # Sleep for 5 minutes to avoid duplicate entries for the same hour
+
+    time.sleep(60)  # Check every minute
---
+++ utilities.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pytz import timezone
+
+def is_forex_market_open():
+    now_est = datetime.now(timezone('US/Eastern'))
+    open_time_sunday = datetime.strptime('18:00:00', '%H:%M:%S').time()
+    close_time_friday = datetime.strptime('17:00:00', '%H:%M:%S').time()
+
+    # If it's currently the weekend
+    if now_est.weekday() > 4:
+        if now_est.weekday() == 6:  # if it's Sunday
+            if now_est.time() > open_time_sunday:
+                return True
+            else:
+                return False
+        else:  # if it's Saturday
+            return False
+    else:  # if it's a weekday
+        if now_est.weekday() == 4:  # if it's Friday
+            if now_est.time() > close_time_friday:
+                return False
+            else:
+                return True
+        else:  # if it's Monday-Thursday
+            return True
