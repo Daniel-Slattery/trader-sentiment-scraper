5ea5941cd7e6fe3e7eb56274ddc9bf70d1656b7b
Daniel Slattery <daniel.slattery1@gmail.com>
2023-08-13 18:31:47 -0400

    added dynamic adaption of headers

--- scraper.py
+++ scraper.py
@@ -14,28 +14,61 @@ import requests
 # Get the URLs from the environment variables
 url = os.environ.get('SENTIMENT_URL')
 monitoring_url = os.environ.get('MONITORING_URL')
+headerMismatchURL = os.environ.get('HEADER_MISMATCH_URL')
+
 
 def scrape(driver):
     # Make a request to the website
     driver.get(url)
 
     # Wait for the JavaScript to load the data
-    time.sleep(5)
+    time.sleep(10)
 
     # Now the data should be loaded we can grab it
     headers = [header.text for header in driver.find_elements(By.CLASS_NAME, 'SentimentRowCaption')]
     values = [value.text for value in driver.find_elements(By.CLASS_NAME, 'SentimentValueCaptionLong')]
 
+    if len(headers) != len(values):
+        print("Mismatch in number of headers and values.")
+        return
+
     # Get the next hour as the timestamp
     next_hour = (datetime.now() + timedelta(hours=1)).replace(minute=0, second=0)
-    data_row = [next_hour.strftime('%Y-%m-%d %H:%M')] + values
 
-    with open('Sentiment.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        if f.tell() == 0:  # file is empty, write headers
-            writer.writerow(["Timestamp"] + headers)
-        print(f"Writing item: {data_row}")  # Print each item before writing
-        writer.writerow(data_row)  # adjust this depending on the structure of 'item'
+    # Convert data into dictionary
+    data_dict = {"Timestamp": next_hour.strftime('%Y-%m-%d %H:%M')}
+    for header, value in zip(headers, values):
+        data_dict[header] = value
+
+    # Check if file exists and headers match
+    if os.path.exists('Sentiment.csv'):
+        with open('Sentiment.csv', 'r') as f:
+            existing_headers = next(csv.reader(f))
+            if headers != existing_headers[1:]:  # Skip the "Timestamp" header
+                print("Headers mismatch! Writing to a new CSV file.")
+
+                # Ping headerMismatchURL due to the mismatch
+                try:
+                    requests.get(headerMismatchURL)
+                    print("Notified the headerMismatchURL due to headers mismatch.")
+                except requests.exceptions.RequestException as e:
+                    print("Error pinging headerMismatchURL:", e)
+
+                # Generate a new filename based on the current timestamp
+                new_filename = 'Sentiment_' + datetime.now().strftime('%Y%m%d_%H%M%S') + '.csv'
+                with open(new_filename, 'w', newline='') as new_f:
+                    writer = csv.DictWriter(new_f, fieldnames=["Timestamp"] + headers)
+                    writer.writeheader()
+                    writer.writerow(data_dict)
+                    print(f"Data written to {new_filename}")
+                return
+    else:
+        with open('Sentiment.csv', 'a', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=["Timestamp"] + headers)
+            if f.tell() == 0:  # file is empty, write headers
+                writer.writeheader()
+            print(f"Writing item: {data_dict}")  # Print each item before writing
+            writer.writerow(data_dict)
 
 while True:
     if is_forex_market_open():
